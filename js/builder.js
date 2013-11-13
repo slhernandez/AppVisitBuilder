a2e07436d9c7c95d26ccfdde1a85029f405a9013
Steve Hernandez <slhernandez@gmail.com>
2013-11-13 15:26:13 -0600

    Can hide and show child questions (1 level)

--- js/builder.js
+++ js/builder.js
@@ -1,5 +1,23 @@
 var builder = {
   init: function() {
     console.log('builder.js is loaded...')
+
+    // Setup events
+    $('.btn-yes').on('click', function(e) {
+      if ( $(this).next().is(':hidden') ) {
+        $(this).next().slideDown("200");
+      } else {
+        $(this).next().slideUp("200");
+      }
+    });
+
+    $('.btn-no').on('click', function(e) {
+      if ( $(this).next().is(':hidden') ) {
+        $(this).next().slideDown("200");
+      } else {
+        $(this).next().slideUp("200");
+      }
+    });
+
   }
 }
