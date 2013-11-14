d776f75ff29ce63352454e63728f3457161fdd85
Steve Hernandez <slhernandez@gmail.com>
2013-11-14 13:50:25 -0600

    Now included partials/includes to micro templates.

--- js/builder.js
+++ js/builder.js
@@ -5,21 +5,23 @@ var builder = {
     // Setup events
     $('.btn-yes').on('click', function(e) {
       if ( $(this).next().is(':hidden') ) {
-      // load the YesNoQuestion template
-      _this.renderYesNoQuestion("add");
+        // load the YesNoQuestion template
+        _this.renderYesNoQuestion("add", $(this));
         $(this).next().slideDown("200");
       } else {
         $(this).next().slideUp("200", function(){
-          _this.renderYesNoQuestion("remove");
+        _this.renderYesNoQuestion("remove", $(this));
         });
       }
     });
 
     $('.btn-no').on('click', function(e) {
       if ( $(this).next().is(':hidden') ) {
+        _this.renderSelectQuestion("add", $(this));
         $(this).next().slideDown("200");
       } else {
         $(this).next().slideUp("200");
+        _this.renderSelectQuestion("remove", $(this));
       }
     });
 
@@ -33,12 +35,38 @@ var builder = {
     });
   },
 
-  renderYesNoQuestion: function(action) {
+  renderYesNoQuestion: function(action, context) {
     if (action === 'add') {
-      $('.panel-child-container').append(_.template($('#yes-no-question').html())); 
+      var $panel = context.next('.panel-child-container');
+      $panel.append(this.template($('#yes-no-question-template').html())); 
     } else if (action === 'remove') {
-      $('.yesno-template').remove();
+      var $panel = context;
+      $panel.find('.yesno-template').remove();
     }
+  },
+
+  renderSelectQuestion: function(action, context) {
+    var $panel = context.next('.panel-child-container');
+    if (action === 'add') {
+      $panel.append(this.template($('#select-question-template').html()));
+    } else if (action === 'remove') {
+      $panel.find('.question-template').remove();
+    }
+  },
+
+  // Extend underscore's template() to allow inclusions
+  template: function(str, data) {
+    // match "<% include template-id %>"
+    return _.template(
+      str.replace(
+        /<%\s*include\s*(.*?)\s*%>/g,
+        function(match, templateId) {
+          var el = document.getElementById(templateId);
+          return el ? el.innerHTML : '';
+        }
+      ),
+      data
+    );
   }
 
 }
