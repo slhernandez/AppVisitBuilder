43f4a73da75ef9b5012e9ccb795af98c1eb893f0
Steve Hernandez <slhernandez@gmail.com>
2013-11-14 17:12:55 -0600

    Everything is now using micro-templating.  Parent question dropdown now working.

--- js/builder.js
+++ js/builder.js
@@ -1,21 +1,32 @@
 var builder = {
   init: function() {
     console.log('builder.js is loaded...')
+
+    // Render template mapping  
+    this.questionTemplate = {
+      "Menu of Steps": "menu-of-steps-template",
+      "Header Text": "select-question-template",
+      "Yes-No Question": "yes-no-question-template",
+      "Default": "select-question-template"
+    }
+
     var _this = this;
-    // Setup events
-    $('.btn-yes').on('click', function(e) {
+
+    // Setup click events
+    $('.work-container').on('click', '.btn-yes', function(e) {
       if ( $(this).next().is(':hidden') ) {
-        // load the YesNoQuestion template
-        _this.renderYesNoQuestion("add", $(this));
+        // load child question template
+        //_this.renderYesNoQuestion("add", $(this));
+        _this.renderChildQuestionContainer("add", $(this));
         $(this).next().slideDown("200");
       } else {
         $(this).next().slideUp("200", function(){
-        _this.renderYesNoQuestion("remove", $(this));
+        //_this.renderYesNoQuestion("remove", $(this));
         });
       }
     });
 
-    $('.btn-no').on('click', function(e) {
+    $('.work-container').on('click', '.btn-no', function(e) {
       if ( $(this).next().is(':hidden') ) {
         _this.renderSelectQuestion("add", $(this));
         $(this).next().slideDown("200");
@@ -25,14 +36,45 @@ var builder = {
       }
     });
 
-    $('.dropdown-menu').on('click', 'li', function(e) {
+    // Click event for the dropdown menu 
+    // Parent Question selection
+    $('.work-heading').on('click', '.dropdown-menu li', function(e) {
+
+
       e.preventDefault();
       console.log('selected ', $(this).text());
       // Update button label
       var selectedItem = $(this).text();
       $(this).parent().prev().prev().text(selectedItem);
       console.log('load the question form...');
+      _this.renderParentQuestion(_this.questionTemplate[selectedItem], $(this));
+
     });
+
+    // Initialize the page with a parent question container
+    this.renderParentQuestionContainer();
+  },
+
+  renderParentQuestionContainer: function(init) {
+    $('.work-heading').append(this.template($('#parent-question-menu-template').html()));    
+    $('.work-container').append(this.template($('#parent-question-container-template').html()));
+    $('.work-container .question-template').append(this.template($('#select-question-template').html()));
+  },
+
+  renderParentQuestion: function(questionTemplate, context) {
+    var $questionTarget = context.closest('.work-heading').next();
+    $questionTarget.find('.question-template').empty();
+    $questionTarget.find('.question-template').append(this.template($('#'+questionTemplate).html()));
+  },
+
+  renderChildQuestionContainer: function(action, context) {
+    var $childPanel = context.next('.panel-child-container');
+    if (action === 'add') {
+      $childPanel.append(this.template($('#child-question-container-template').html()));
+      $childPanel.append(this.template($('#select-question-template').html()));
+
+    } else if (action === 'hide') {
+    }
   },
 
   renderYesNoQuestion: function(action, context) {
