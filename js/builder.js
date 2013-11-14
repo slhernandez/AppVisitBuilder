d4f0390ce789e5d1db6d78221f78f18cc70223a0
Steve Hernandez <slhernandez@gmail.com>
2013-11-13 18:26:03 -0600

    Now updating the dropdown menus

--- js/builder.js
+++ js/builder.js
@@ -19,5 +19,16 @@ var builder = {
       }
     });
 
+    $('.dropdown-menu').on('click', 'li', function(e) {
+      e.preventDefault();
+      console.log('selected ', $(this).text());
+      // Update button label
+      var selectedItem = $(this).text();
+      $(this).parent().prev().prev().text(selectedItem);
+      console.log('load the question form...');
+
+    });
+
+
   }
 }
