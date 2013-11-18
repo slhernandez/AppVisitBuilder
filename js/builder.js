d362b5d70dd33d8f02f1499dfd1bec49bc4ea476
Steve Hernandez <slhernandez@gmail.com>
2013-11-18 15:02:15 -0600

    Updated the preview markup and update. Entire body markup is now in micro-templating.

--- js/builder.js
+++ js/builder.js
@@ -13,7 +13,7 @@ var builder = {
     var _this = this;
 
     // Setup click events - YES button
-    $('.work-container').on('click', '.btn-yes', function(e) {
+    $('body').on('click', '.work-container .btn-yes', function(e) {
       if ( $(this).next().is(':hidden') ) {
         // load child question template
         _this.renderChildQuestionContainer("add", $(this));
@@ -26,7 +26,7 @@ var builder = {
     });
 
     // Setup click events - NO button
-    $('.work-container').on('click', '.btn-no', function(e) {
+    $('body').on('click', '.work-container .btn-no', function(e) {
       if ( $(this).next().is(':hidden') ) {
         _this.renderChildQuestionContainer("add", $(this));
         $(this).next().slideDown("200");
@@ -37,7 +37,7 @@ var builder = {
     });
 
     // Setup click events for single group list
-    $('.work-container').on('click', '.list-group .list-group-item', function(e) {
+    $('body').on('click', '.work-container .list-group .list-group-item', function(e) {
 
         if ($(this).hasClass('list-add-item')) {
           // TODO:
@@ -57,17 +57,18 @@ var builder = {
 
     // Click event for the Parent Question dropdown menu 
     // Parent question selection
-    $('.work-heading').on('click', '.dropdown-menu li', function(e) {
+    $('body').on('click', '.work-heading .dropdown-menu li', function(e) {
       e.preventDefault();
       // Update button label
       var selectedItem = $(this).text();
       $(this).parent().prev().prev().text(selectedItem);
+      // Render the builder question
       _this.renderParentQuestion(_this.questionTemplate[selectedItem], $(this));
     });
 
     // Click event for the child question dropdown menu
     // Child question selection
-    $('.work-container').on('click', '.dropdown-menu li', function(e) {
+    $('body').on('click', '.work-container .dropdown-menu li', function(e) {
       e.preventDefault();
       // Update button label
       var selectedItem = $(this).text();
@@ -75,11 +76,33 @@ var builder = {
       _this.renderChildQuestion(_this.questionTemplate[selectedItem], $(this));
     });
 
+    // Add Another Question click event 
+    $('body').on('click', '.add-question-container .add-question', function(e) {
+      e.preventDefault();
+      alert('this worked!');
+      // Create a new 
+    });
+
+    // Render the navbar
+    this.renderNavbar();
+
+    // Render the question base markup
+    this.renderQuestionBase();
+
     // Initialize the page with a parent question container
     this.renderParentQuestionContainer();
   },
 
+  renderNavbar: function() {
+    $('body').append(this.template($('#navbar-template').html()));
+  },
+
+  renderQuestionBase: function() {
+    $('body').append(this.template($('#question-base-template').html()));
+  },
+
   renderParentQuestionContainer: function(init) {
+    // Includes both rendering the builder and preview question containers.
     $('.work-heading').append(this.template($('#parent-question-menu-template').html()));    
     $('.work-container').append(this.template($('#parent-question-container-template').html()));
     $('.work-container .question-template').append(this.template($('#select-question-template').html()));
@@ -90,9 +113,20 @@ var builder = {
   },
 
   renderParentQuestion: function(questionTemplate, context) {
-    var $builderTarget = context.closest('.work-heading').next();
+    var $flipTarget = context.closest('.flipper');
+    var $builderTarget = $flipTarget.find('.work-container');
+    var $previewTarget = $flipTarget.find('.preview-container');
+
+    // Remove any existing question markup.
     $builderTarget.find('.question-template').empty();
+    $previewTarget.find('.question-template').empty();
+
+    // Render Builder Template
     $builderTarget.find('.question-template').append(this.template($('#'+questionTemplate).html()));
+    // Render Preview Template
+    var previewTemplate = questionTemplate + '-preview';
+    $previewTarget.find('.question-template').append(this.template($('#'+previewTemplate).html()));
+
   },
 
   renderChildQuestionContainer: function(action, context) {
